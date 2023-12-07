0025d23ec254ef8e1481e1f8cfdb310a6033bfab
StudentAM <studentam@tech-hp.metroed.local>
2023-12-07 10:25:52 -0800

    hi

--- JavaScript-Assignment5/1.js
+++ JavaScript-Assignment5/1.js
@@ -82,7 +82,9 @@ var quantityElement = document.getElementById("quantity")
 var total = document.getElementById("total");
 var checkout = document.getElementById("checkout");
 var list = document.getElementById ("list"); 
-var statusMsg = document.getElementById ("wrongNum")
+var statusMsg = document.getElementById ("wrongNum");
+var runningTotal = 0;
+var subtotal = document.getElementById("subtotal")
 const TaxRate = 9.25 / 100
 
 addToCartButton.addEventListener("click", addItemToCart)
@@ -95,34 +97,63 @@ function addItemToCart(){
     var quantity = quantityInput.value;
 
     if(items.hasOwnProperty(barcodeNum)){
+     //create variable
         let itemsInfo = items[barcodeNum];
         let numOfItems = quantity;
+        let tempPrice = numOfItems * parseFloat(itemsInfo.price);
+
+        checkCart(itemsInfo);
+        let foundItem = checkCart(itemsInfo);
+        console.log(foundItem);
+
+
+        if(foundItem){
+          // adds a quantity if the item is the same
+          let oldQuantity = foundItem.querySelector(".quanOfItem").innerText;
+          let newQuantity = parseInt(oldQuantity) + parseInt(quantity);
+          // oldQuantity = newQuantity;
+          foundItem.querySelector(".quanOfItem").innerText = newQuantity
+          updateTotal(tempPrice);
+          return;
+        }
+
+
+        updateTotal(tempPrice);
         //creates new div
         var itemsElement = document.createElement("div");
-        itemsElement.classList.add("items");
         itemsElement.classList.add("itemsContainer")
          //Creates new p element
 
+         //Create a name p element
         var itemName = document.createElement("p");
         itemName.innerText = itemsInfo.name;
+        itemName.classList.add("nameOfItem");
 
+        //Create a price p element
         var itemPrice = document.createElement("p");
         itemPrice.innerText = itemsInfo.price;
-        
+        itemPrice.classList.add("priceOfItem");
+  
+        //Create a quantity p element
         var itemQuantity = document.createElement ("p");
         itemQuantity.innerText = numOfItems;
+        itemQuantity.classList.add("quanOfItem");
 
 
-        total += parseFloat(itemsInfo) * parseFloat(itemQuantity)
+     //    total.innerText += parseFloat(itemsInfo.price) * parseFloat(itemQuantity)
 
-
-
+        //takes the variable name and makes apart of the item element
         itemsElement.append(itemName);
         itemsElement.append(itemPrice);
         itemsElement.append(itemQuantity);
 
         list.append(itemsElement);
+        
+
+        console.log(updateTotal)
+       
     }
+    //show up not in system if the barcode isnt there
     else{
         statusMsg.innerText = "The item is not in the system"
     }
@@ -130,3 +161,36 @@ function addItemToCart(){
    
 }
 
+function updateTotal (tempPrice){
+     //adds the total of the added item to the running total
+     runningTotal += tempPrice;
+     total.innerText = "Total: $" + runningTotal;
+
+}
+
+function checkCart(itemsInfo){
+     // gets info about all of the items in cart
+     let itemsInCart = document.querySelectorAll(".itemsContainer");
+     
+     //goes through all of the items in cart and updates the item
+     for(let i = 0; i < itemsInCart.length; i++){
+          let nameOfItem = itemsInCart[i].querySelector(".nameOfItem");
+          if(itemsInfo.name === nameOfItem.innerText)
+          return itemsInCart[i];
+     }
+}
+
+//adds sales tax to the total
+function checkoutCart(){
+     //create sales tax
+     let salesTax = 0.0925;
+     //add sales tax to the running total
+     let finalTotal = runningTotal + (runningTotal * salesTax);
+     runningTotal = 0
+     
+     //add final total to the p element
+     subtotal.innerText = "grand total including tax is $" + finalTotal.toFixed(2);
+     
+}
+
+document.getElementById("checkoutButton").addEventListener("click", checkoutCart)
